dcff464107d9b54e339210f3aaeda32853f50e53
imolina212 <86175612+imolina212@users.noreply.github.com>
2021-12-13 22:44:32 -0500

    The End

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route , Navigate, useNavigate} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import React from "react";
 
 /** COMPONENTS */
